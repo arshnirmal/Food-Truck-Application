f059db2c25c8c6fed49416ddac032627a451a6ae
arshnirnal <arshnirnal11@gmail.com>
2024-02-19 20:40:23 +0530

    auth service

--- backend/src/main/java/dev/arshnirmal/foodtruckbackend/services/JwtService.java
+++ backend/src/main/java/dev/arshnirmal/foodtruckbackend/services/JwtService.java
@@ -1,10 +1,31 @@
 package dev.arshnirmal.foodtruckbackend.services;
 
+import io.jsonwebtoken.Claims;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY = "04ed405e4f668ae53d280927825a0f872794ae37cfc765c169069bc81149abbd";
     public String extractUserName(String token){
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    private SecretKey getSecretKey(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET_KEY));
+    }
+
+    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
+        return claimsResolver.apply(extractAllClaims(token));
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser().verifyWith(getSecretKey()).build().parseSignedClaims(token).getPayload();
     }
 }
