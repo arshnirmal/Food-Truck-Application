1260a35ae6b2ae6583f04ddc10a31b458b5ef114
arshnirmal <arshnirmal11@gmail.com>
2024-04-15 22:53:10 +0530

    splashscreen

--- frontend/lib/screens/onboarding/splash_screen.dart
+++ frontend/lib/screens/onboarding/splash_screen.dart
@@ -2,42 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:food_truck/resources/res.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // body: SafeArea(
-      //   child: Stack(
-      //     alignment: Alignment.center,
-      //     children: [
-      //       SvgPicture.asset(
-      //         Resources.images.appName,
-      //         height: 60,
-      //       ),
-      //       Column(
-      //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      //         children: [
-      //           Align(
-      //             alignment: Alignment.centerLeft,
-      //             child: SvgPicture.asset(
-      //               Resources.images.splashScreenTop,
-      //               height: 100,
-      //             ),
-      //           ),
-      //           Align(
-      //             alignment: Alignment.centerRight,
-      //             child: SvgPicture.asset(
-      //               Resources.images.splashScreenBottom,
-      //               height: 300,
-      //             ),
-      //           ),
-      //         ],
-      //       ),
-      //     ],
-      //   ),
-      // ),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            SvgPicture.asset(
+              Resources.images.appName,
+              height: 60,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: SvgPicture.asset(
+                    Resources.images.splashScreenTop,
+                    height: 100,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: SvgPicture.asset(
+                    Resources.images.splashScreenBottom,
+                    height: 250,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
