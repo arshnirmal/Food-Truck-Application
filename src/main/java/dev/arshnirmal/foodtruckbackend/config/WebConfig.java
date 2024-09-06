e431be0be4225ed7cc1bd0e04635d38265244fe9
arshnirmal <arshnirmal11@gmail.com>
2024-09-06 23:29:33 +0530

    github actions

--- src/main/java/dev/arshnirmal/foodtruckbackend/config/WebConfig.java
+++ src/main/java/dev/arshnirmal/foodtruckbackend/config/WebConfig.java
@@ -13,7 +13,8 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("192.168.1.1")
+                        .allowedOrigins("*")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
